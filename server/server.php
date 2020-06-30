0ec5ec7b7d827c771c8e43c53c63d57bbd190bf7
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2020-06-29 23:00:22 -0300

    Criar diretório de gravação automaticamente caso não exista

--- server/server.php
+++ server/server.php
@@ -1,7 +1,12 @@
 <?php
    $data = 'data.txt';
+   $recDir = 'records';
+   clearstatcache();
    if(!file_exists($data)){
       file_put_contents($data, '', FILE_APPEND);
+   }
+   if(!file_exists($recDir)){
+      mkdir($recDir);
    }
    if(isset($_GET['cmd'])){
       $command = $_GET['cmd'];
@@ -11,7 +16,7 @@
             $temp = str_split(file_get_contents($data, FILE_TEXT), 1);
             $temp[$id] = '0';
             file_put_contents($data, implode('', $temp), FILE_TEXT);
-            if(unlink('records/rec-'.$id.'.oga')){
+            if(unlink($recDir.'/rec-'.$id.'.oga')){
                echo '0';
             }else{
                echo '1';
@@ -24,7 +29,7 @@
    }else{
       $file = file_get_contents('php://input');
       $id = filesize($data);
-      file_put_contents('records/rec-'.$id.'.oga', $file, FILE_APPEND);
+      file_put_contents($recDir.'/rec-'.$id.'.oga', $file, FILE_APPEND);
       file_put_contents($data, '1', FILE_TEXT|FILE_APPEND);
       echo $id;
    }
